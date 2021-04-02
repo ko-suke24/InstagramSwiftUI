9fb4877888e38e38dcf094e7a1a77e9fde96401d
ko-suke24 <kosuke0322@gmail.com>
2021-04-02 14:24:56 +0900

    Initial Commit

---
+++ instagramSwiftUI/instagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramSwiftUIApp.swift
+//  instagramSwiftUI
+//
+//  Created by 佐藤航輔 on 2021/04/02.
+//
+
+import SwiftUI
+
+@main
+struct instagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
