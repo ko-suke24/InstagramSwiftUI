0da56b5ed6c9397ebd42603793368ac0f69a15a7
ko-suke24 <kosuke0322@gmail.com>
2021-04-12 15:57:41 +0900

    Upload Post Page

--- instagramSwiftUI/View/UploadPost/UploadPostView.swift
+++ instagramSwiftUI/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
+    
     var body: some View {
-        Text("UploadPost")
+        VStack {
+            if postImage != nil {
+                Button(action: {}, label: {
+                    Image(systemName: "camera.circle")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .clipped()
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                 
+            })
+            }else {
+                HStack {
+                    Image("10")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    TextField("aaaaa", text:$captionText)
+                }.padding()
+                
+                Button(action: {}, label: {
+                    Text("シェアする")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }).padding()
+            }
+            
+            Spacer()
+        }
     }
 }
 
