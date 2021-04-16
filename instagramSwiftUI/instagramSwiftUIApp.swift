01fee236d25d4baf2b19b29a7d50ec7c8444ad29
ko-suke24 <kosuke0322@gmail.com>
2021-04-16 22:01:03 +0900

    Firebase setup

--- instagramSwiftUI/instagramSwiftUIApp.swift
+++ instagramSwiftUI/instagramSwiftUIApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct instagramSwiftUIApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            LoginView()
         }
     }
 }
