74ddffc295eab8d574077665a8e6e0d2709ca283
ko-suke24 <kosuke0322@gmail.com>
2021-04-19 14:06:12 +0900

    Fetching All Users

---
+++ instagramSwiftUI/ViewModel/SearchViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  SearchViewModel.swift
+//  instagramSwiftUI
+//
+//  Created by 佐藤航輔 on 2021/04/19.
+//
+
+import SwiftUI
+
+class SearchViewModel: ObservableObject {
+    @Published var users = [User]()
+    
+    init() {
+        fetchUsers()
+    }
+    
+    func fetchUsers() {
+        COLLECTION_USERS.getDocuments { snapshot, _ in
+            guard let documents = snapshot?.documents else { return }
+            self.users = documents.compactMap({ try? $0.data(as: User.self) })
+        
+            print(self.users)
+        }
+    }
+}
